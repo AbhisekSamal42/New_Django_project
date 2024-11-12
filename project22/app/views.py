7120aed7d646e40b38a2f11942be6780bfe4c70a
AbhisekSamal42 <samalabhisek42@gmail.com>
2024-11-12 19:57:14 +0530

    User_Registration_Process

--- project22/app/views.py
+++ project22/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from app.forms import *
+
+def registration(request):
+    EUFO=UserForm()
+    EPFO=ProfileForm()
+    D={'EUFO':EUFO,'EPFO':EPFO}
+    return render(request,'registration.html',D)
