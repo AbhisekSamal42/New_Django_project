050879c66236451bcd3e5d042e05030a5dbaab7e
AbhisekSamal42 <samalabhisek42@gmail.com>
2024-11-13 23:44:27 +0530

    User_Registration_Process

--- project22/app/models.py
+++ project22/app/models.py
@@ -6,6 +6,6 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    User_name=models.OneToOneField(User,on_delete=models.CASCADE)
-    Address=models.TextField()
-    Profile_photo=models.ImageField()
+    username=models.OneToOneField(User,on_delete=models.CASCADE)
+    address=models.TextField()
+    profile_photo=models.ImageField()
